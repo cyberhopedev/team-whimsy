b893f7666d28e42e4dc0f865ce946a5fe4a6513b
hkspence <hkspence@ncsu.edu>
2026-02-19 11:58:47 -0500

    [Implementation] Basic player movement script

--- Moth's Ascent/Assets/Scripts/Player.cs
+++ Moth's Ascent/Assets/Scripts/Player.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+/// <summary> 
+/// Represents a player, providing basic movement
+/// </summary>
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float movementSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    /// <summary> 
+    /// Start is called once before the first execution of Update after the MonoBehaviour is created
+    /// </summary>
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    /// <summary> 
+    /// Update is called once per frame
+    /// </summary>
     void Update()
     {
-        
+        rb.linearVelocity = moveInput * movementSpeed;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
     }
 }
