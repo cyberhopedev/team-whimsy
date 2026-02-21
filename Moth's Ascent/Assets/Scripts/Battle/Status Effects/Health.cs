0f1557691befa5ce3e3c7839e7dc0aac7190258b
dmbrow28 <dmbrow28@ncsu.edu>
2026-02-21 06:09:34 -0500

    Battle Menu base, needs more dynamic functionality next

---
+++ Moth's Ascent/Assets/Scripts/Battle/Status Effects/Health.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehavior
+{
+    public int health;
+
+    public int max_health;
+
+    public Slider healthBar;
+
+    // Call this before a fight, player can only take max_hits before dying
+    protected void Start(int max_hits)
+    {
+        max_health = max_hits;
+        health = max_hits;
+
+        slider.maxValue = max_health;
+        slider.value = health;
+    }
+
+    // Called once per frame
+    protected void Update()
+    {
+        
+    }
+
+    // Call when player or enemy attacks, stronger hits might be worth more than 1 num_hits
+    public void TakeDamage(int num_hits)
+    {
+        health -= amount;
+        slider.value = health;
+
+        // Die condition
+        if (health <= 0)
+        {
+            // change sprite, destroy object ?
+        }
+    }
+
+    // Call when character gets healed?
+    public void GetHealth(int num_hits)
+    {
+        // Make sure not to go over max health
+        if (health + num_hits >= max_health)
+        {
+            health = max_health;
+        } else
+        {
+            health += num_hits;
+        }
+        slider.value = health;
+    }
+
+    // Call to get health back to 100%
+    public void restoreHealth()
+    {
+        health = max_health;
+        slider.value = health;
+    }
+}
---
+++ Moth's Ascent/Assets/Scripts/Battle/UI/BattleMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleMenu : MonoBehaviour
+{
+    public Text attack1Text;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Lists all the current attack options for the player
+    public void listAttacks()
+    {
+        attack1Text.text = "temp";
+    }
+}
