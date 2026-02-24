f99e890b568279489d9a537bfdfab4b5a4a5850f
hkspence <hkspence@ncsu.edu>
2026-02-24 16:52:54 -0500

    [Implementation] Added Beta-Battle-Scene and refactored main scene to Beta-Overworld-Scene

--- Moth's Ascent/Assets/Scripts/Battle/EnemyEncounter.cs
+++ Moth's Ascent/Assets/Scripts/Battle/EnemyEncounter.cs
@@ -20,11 +20,12 @@ public class EnemyEncounter : MonoBehaviour
     {
         // If the player collides with an enemy, disable free movement and enable the battle system
         if (c.TryGetComponent(out PlayerController pc))
-        {
+        {   
             pc.enabled = false;
             pc.GetComponent<PlayerBattler>().enabled = true;
             BattleSystem.Instance.StartBattle(encounterEnemies);
 
+            // TODO: Handle a way to switch into the battle scene rather than setting the tilemap false
             // Also, make sure to the hide the tilemap we have in the overworld camera
             gameObject.SetActive(false);
         }
