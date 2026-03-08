e64763a6585ec93a903b7698aed3bd94bae508b3
hkspence <hkspence@ncsu.edu>
2026-03-07 22:47:04 -0500

    [Implementation] Refactored Battler.cs to account for status effects Poison and Spore Sickness

--- Moth's Ascent/Assets/Scripts/Battle/Battlers/Battler.cs
+++ Moth's Ascent/Assets/Scripts/Battle/Battlers/Battler.cs
@@ -21,6 +21,8 @@ public abstract class Battler : MonoBehaviour
     public Slider healthBar;
     // Speed Stat
     public int speedStat;
+    // List of active status effects on the battler
+    public List<StatusEffectInstance> activeStatusEffects = new List<StatusEffectInstance>();
 
     /// <summary> 
     /// Constructs a Battler with their assosciated stats
@@ -47,10 +49,12 @@ public abstract class Battler : MonoBehaviour
     }
 
     /// <summary> 
-    /// Calls the BattleSystem to begin the Battler's turn
+    /// Calls the BattleSystem to begin the Battler's turn. Processes
+    /// the status effects that are currently active
     /// </summary>
     public void BeginTurn()
     {
+        ProcessStatusEffects();
         StartTurn();
     }
 
@@ -60,4 +64,62 @@ public abstract class Battler : MonoBehaviour
     public abstract void TakeDamage(int amount);
     protected abstract void StartTurn();
     protected abstract void EndTurn();
+
+    /// <summary>
+    /// Called at the start of each turn to tick down status effects
+    /// and apply their damage to the inflicted battler. If the battler dies,
+    /// the status effect is removed and battler's death is handled by TakeDamage()
+    /// </summary>
+    protected void ProcessStatusEffects()
+    {
+        foreach(StatusEffectInstance se in activeStatusEffects.ToList())
+        {
+            // Tick damage
+            if (se.damagePerTurn > 0)
+            {
+                TakeDamage(se.damagePerTurn);
+                Debug.Log(gameObject.name + " takes " + se.damagePerTurn + " damage from " + se.statusEffect.name);
+            }
+
+            // Decrement remaining turns and remove if expired
+            se.remainingTurns--;
+            if (se.remainingTurns <= 0)
+            {
+                activeStatusEffects.Remove(se);
+                Debug.Log(se.statusEffect.name + " on " + gameObject.name + " has wore off!");
+            }
+        }
+    }
+
+    /// <summary>
+    /// Applies a new status effect to the battler, ensuring that the same effect type cannot stack. 
+    /// If the same effect type is already active, the new effect will not be applied
+    /// </summary>
+    /// <param name="newEffect">The new status effect to apply</param>
+    public void ApplyStatusEffect(StatusEffectInstance newEffect)
+    {
+        // Make sure the same effect type cannot stack, instead make it do nothing
+        foreach (var existing in activeEffects)
+        {
+            if (existing.type == effect.type)
+            {
+                Debug.Log(gameObject.name + " already has " + effect.statusEffect.name + " and cannot be applied again!");
+                return;
+            }
+        }
+
+        // Apply the new effect
+        activeEffects.Add(effect);
+    }
+
+    /// <summary>
+    /// Helper method to check if the battler has a specific status effect type active. 
+    /// Returns true if the effect is active, false otherwise.
+    /// </summary>
+    /// <param name="type">The type of status effect to check for</param>
+    /// <returns>True if the effect is active, false otherwise</returns>
+    public bool hasStatusEffect(StatusEffectType type)
+    {
+        return activeStatusEffects.Any(se => se.statusEffect.type == type);
+    }
 }
