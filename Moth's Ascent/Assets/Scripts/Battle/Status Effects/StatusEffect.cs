3a1dd458886deed4f1c922e725576ce52989b2db
hkspence <hkspence@ncsu.edu>
2026-03-07 22:26:08 -0500

    [Implementation] Added StatusEffect.cs to hold info for effects

---
+++ Moth's Ascent/Assets/Scripts/Battle/Status Effects/StatusEffect.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+/// <summary>
+/// Enum for status effects that can be applied to player or enemy
+/// in battle
+/// </summary>
+public enum StatusEffectType
+{
+    NONE,
+    POISON,
+    SPORE_SICKNESS
+}
+/// <summary>
+/// 
+/// </summary>
+[System.Serializable]
+public class StatusEffectInstance
+{
+    // Type of status effect
+    public StatusEffectType type;
+    // Amount of damage dealt to the affected battler at the end of each turn
+    public int damagePerTurn;
+    // Number of turns remaining for the status effect
+    public int turnsRemaining;
+
+    /// <summary>
+    /// Constructor for a status effect instance. 
+    /// Initializes the type, damage per turn, and duration of the effect.
+    /// </summary>
+    /// <param name="type">The type of the status effect</param>
+    /// <param name="damagePerTurn">The amount of damage dealt per turn</param>
+    /// <param name="duration">The number of turns the effect lasts</param>
+    public StatusEffectInstance(StatusEffectType type, int damagePerTurn, int duration)
+    {
+        this.type = type;
+        this.damagePerTurn = damagePerTurn;
+        this.turnsRemaining = duration;
+    }
+}
