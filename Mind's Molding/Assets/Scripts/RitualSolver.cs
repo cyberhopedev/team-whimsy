f385403a8a1cfecf72c716b49dedf098c8248627
hkspence <hkspence@ncsu.edu>
2026-03-24 13:33:07 -0400

    [Implementation][Skeleton] Added skeleton for RitualSolver.cs

---
+++ Mind's Molding/Assets/Scripts/RitualSolver.cs
@@ -0,0 +1,30 @@
+
+public class RitualResolver : MonoBehaviour
+{
+    // Magic circle puzzle flag
+    [SerializeField] private MagicCirclePuzzle circlePuzzle;
+    // Incantation puzzle flag
+    [SerializeField] private IncantationPuzzle incantationPuzzle;
+    // Materials puzzle flag
+    [SerializeField] private MaterialsPuzzle materialsPuzzle;
+
+    bool hasMagicCircle, hasIncantation, hasMaterials;
+
+    /// <summary>
+    /// Called when the script instance is being loaded. Registers event listeners for puzzle completion.
+    /// </summary>
+    public void OnEnable()
+    {
+        circlePuzzle.OnPuzzleSolved.AddListener(() => { pass; });
+        incantationPuzzle.OnPuzzleSolved.AddListener(() => { pass; });
+        materialsPuzzle.OnPuzzleSolved.AddListener(() => { pass; });
+    }
+
+    /// <summary>
+    /// Checks if the player has solved all of the puzzles
+    /// </summary>
+    public void Check()
+    {
+        
+    }
+}
