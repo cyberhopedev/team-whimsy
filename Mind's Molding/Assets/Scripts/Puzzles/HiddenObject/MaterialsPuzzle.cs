42d3e2eb8f5fc02844592b5c3b2404b7bce3f1a6
dmbrow28 <dmbrow28@ncsu.edu>
2026-04-06 15:06:12 -0400

    materials interface working

--- Mind's Molding/Assets/Scripts/Puzzles/HiddenObject/MaterialsPuzzle.cs
+++ Mind's Molding/Assets/Scripts/Puzzles/HiddenObject/MaterialsPuzzle.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 /// <summary>
 /// Class that holds the logic for the Materials Puzzle.  
@@ -11,7 +13,11 @@ public class MaterialsPuzzle : BasePuzzle
 {
     public List<ItemData> inventory = new();
     [SerializeField] private ItemData mudItem, bonesItem, chalkyMixture;
-    [SerializeField] private ItemData rootItem;
+    // [SerializeField] private ItemData rootItem;
+    // Screen visuals
+    [SerializeField] private TextMeshProUGUI progress;
+    [SerializeField] private Image item1;
+    [SerializeField] private Image item2;
 
     /// <summary>
     /// Adds item to the inventory   
@@ -20,6 +26,18 @@ public class MaterialsPuzzle : BasePuzzle
     public void AddItem(ItemData item)
     {
         inventory.Add(item);
+        // Visually put item in screen box
+        progress.text = inventory.Count.ToString() + "/2";
+        if (inventory.Count == 1)
+        {
+            item1.sprite = item.icon;
+        } 
+        else
+        {
+            item2.sprite = item.icon;
+        }
+        
+
         TryCraft();
     }
 
@@ -35,7 +53,7 @@ public class MaterialsPuzzle : BasePuzzle
             inventory.Remove(mudItem);
             inventory.Remove(bonesItem);
             inventory.Add(chalkyMixture);
-            DialogueManager.Instance.TriggerWhim("crafted_chalky_mixture");
+            // DialogueManager.Instance.TriggerWhim("crafted_chalky_mixture");
         }
 
         if(CheckSolution())
@@ -44,9 +62,17 @@ public class MaterialsPuzzle : BasePuzzle
         }
     }
 
+    // win result
+    public void WinResult()
+    {
+        Debug.Log("You win ! ");
+        // item1.sprite = chalkyMixture.icon;
+        // item2.sprite = Resources.Load<Sprite>("Sprites/TransparentSprite");
+    }
+
     /// <summary>
     /// Checks if the player's inventory has the chalky mixture and the root item    
     /// </summary>
     public override bool CheckSolution() => 
-        inventory.Contains(chalkyMixture) && inventory.Contains(rootItem);
+        inventory.Contains(chalkyMixture); // && inventory.Contains(rootItem);
 }
