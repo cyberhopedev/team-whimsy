8efd911110d59ce70fdc5a53bc7469176fdfb359
hkspence <hkspence@ncsu.edu>
2026-04-14 12:36:37 -0400

    [Implementation] Tile.cs initial implementation completed

--- Moth's Management/Assets/Scripts/Managers/TileManager.cs
+++ Moth's Management/Assets/Scripts/Managers/TileManager.cs
@@ -1,6 +1,10 @@
 using UnityEngine;
 using System.Collections.Generic;
+
+/// <summary>
+/// Manages and intializes the tiles within the grid for the gameplay
+/// </summary>
 public class TileManager : MonoBehaviour
 {
-    
+    public static TileManager Instance;
 }
