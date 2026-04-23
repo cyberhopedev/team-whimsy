ae7c0e143447667ba389f56fe1481e7fba9ac36c
dmbrow28 <dmbrow28@ncsu.edu>
2026-04-23 11:54:40 -0400

    Added more camera stuff

--- Moth's Management/Assets/Scripts/Managers/TileManager.cs
+++ Moth's Management/Assets/Scripts/Managers/TileManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 /// <summary>
 /// Manages and intializes the tiles within the grid/environment for the gameplay
@@ -99,17 +100,39 @@ public class TileManager : MonoBehaviour
         BuildingManager.Instance.PlaceBuilding(centerTile, BuildingManager.Instance.cottagePrefab);
     }
 
+    // Scale canvas for different resolutions
+    void InitCanvas()
+    {
+        Canvas canvas = FindAnyObjectByType<Canvas>();
+        CanvasScaler scaler = canvas.GetComponent<CanvasScaler>();
+
+        scaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+        scaler.referenceResolution = new Vector2(2560, 1440);
+        scaler.screenMatchMode = CanvasScaler.ScreenMatchMode.MatchWidthOrHeight;
+        scaler.matchWidthOrHeight = 1;
+    }
+
     // This method makes sure the camera will show all tiles
     void FitCamera()
     {
         Camera cam = Camera.main;
-        cam.transform.position = new Vector3(width / 2f + 2f, height / 2f, -10f);
 
         float verticalSize = height / 2f;
         float horizontalSize = (width / 2f) / cam.aspect;
+        cam.orthographicSize = Mathf.Max(verticalSize, horizontalSize) + 0.5f;
 
-        // Use whichever is larger to ensure that all tiles are visible
-        cam.orthographicSize = Mathf.Max(verticalSize, horizontalSize) + 0.5f; // +0.5 for padding
+        // Calculate how wide the full screen is in world units
+        float screenWorldWidth = cam.orthographicSize * 2f * cam.aspect;
+
+        // .25 is to consider the space the UI takes up
+        float uiWidthFraction = 0.25f;
+        float offset = (screenWorldWidth * uiWidthFraction) / 2f;
+
+        // Center on the grid, then shift left to account for UI panel
+        float centerX = (width - 1) / 2f + offset;
+        float centerY = (height - 1) / 2f;
+
+        cam.transform.position = new Vector3(centerX, centerY, -10f);
     }
 
     /// <summary>
